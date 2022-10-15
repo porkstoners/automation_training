7fa2c700c97b7dea651c7cc25273093d53a5e69e
porkstoners <douglas_barker@hotmail.com>
2022-10-14 21:34:12 -0400

    commit changes

--- nornir/netmiko/send_config.py
+++ nornir/netmiko/send_config.py
@@ -6,7 +6,7 @@ from nornir_utils.plugins.functions import print_result
 nr = InitNornir(config_file="/opt/Training/python/automation_training/nornir/config.yaml")
 
 def another_config_test(task):
-    task.run(task=netmiko_send_config, config_commands=['ntp server 11.22.11.22 prefer', 'ntp server 22.33.22.33'])
+    task.run(task=netmiko_send_config, config_commands=['router ospf 15', "network 10.0.0.0/24"])
 
 
 results = nr.run(task=another_config_test)
