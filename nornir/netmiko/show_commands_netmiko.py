faa691fe57e102ad8ab4c01ae5fee13aac7699b5
porkstoners <douglas_barker@hotmail.com>
2022-10-14 21:34:28 -0400

    update scripts

--- nornir/netmiko/show_commands_netmiko.py
+++ nornir/netmiko/show_commands_netmiko.py
@@ -1,11 +1,13 @@
 from nornir import InitNornir, init_nornir
-from nornir_netmiko.tasks import netmiko_send_commands
+from nornir_netmiko.tasks import netmiko_send_command
 from nornir_utils.plugins.functions import print_result
 
 nr = InitNornir(config_file="/opt/Training/python/automation_training/nornir/config.yaml")
+commands = ['show ip int br', 'show version', 'show int status']
 
 def another_show_command_test(task):
-    task.run(task=netmiko_send_commands, command_string="show ip int br")
+    for cmd in commands:
+        task.run(task=netmiko_send_command, command_string=cmd)
 
 results = nr.run(task=another_show_command_test)
 print_result(results)
