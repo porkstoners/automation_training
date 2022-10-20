e480c6eb85a5eb003c8bc24cab2d93ddd6c671ce
porkstoners <douglas_barker@hotmail.com>
2022-10-19 21:08:24 -0400

    update changes network configs

--- nornir/scrapli/send_configs_file.py
+++ nornir/scrapli/send_configs_file.py
@@ -2,7 +2,7 @@ from nornir import InitNornir
 from nornir_scrapli.tasks import send_configs_from_file
 from nornir_utils.plugins.functions import print_result
 
-nr = InitNornir(config_file="config.yaml")
+nr = InitNornir(config_file="/opt/Training/python/automation_training/nornir/config.yaml")
 
 def update_ospf(task):
     task.run(task=send_configs_from_file, file="router_ospf_config.txt")
